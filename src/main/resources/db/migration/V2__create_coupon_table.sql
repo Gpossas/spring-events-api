99e13df146759a0361370daf2d26f862418f771e
Guilherme D. Possas <possasg@gmail.com>
2024-06-17 20:24:13 -0300

    feat: migrate coupon table sql

---
+++ src/main/resources/db/migration/V2__create_coupon_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE coupons (
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    code VARCHAR(50) UNIQUE NOT NULL,
+    discount INTEGER NOT NULL,
+    expiration_date TIMESTAMP NOT NULL,
+    event_id UUID,
+    FOREIGN KEY (event_id) REFERENCES events(id) ON DELETE CASCADE
+);
