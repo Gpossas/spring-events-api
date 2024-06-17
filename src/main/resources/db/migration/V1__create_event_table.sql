57102f8a9b265bbc640624d871eccba0a3924f0f
Guilherme D. Possas <possasg@gmail.com>
2024-06-17 19:19:08 -0300

    feat: create event table SQL

---
+++ src/main/resources/db/migration/V1__create_event_table.sql
@@ -0,0 +1,11 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE TABLE events (
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    description VARCHAR(250),
+    image_url VARCHAR(100),
+    event_url VARCHAR(100),
+    date TIMESTAMP NOT NULL,
+    is_remote BOOLEAN NOT NULL
+);
