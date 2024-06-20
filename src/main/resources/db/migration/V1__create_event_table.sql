7ea81abe4c53f58cd0756415942b135afa1a3111
Guilherme D. Possas <possasg@gmail.com>
2024-06-20 10:01:23 -0300

    fix: image_url size

--- src/main/resources/db/migration/V1__create_event_table.sql
+++ src/main/resources/db/migration/V1__create_event_table.sql
@@ -4,7 +4,7 @@ CREATE TABLE events (
     id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
     description VARCHAR(250),
-    image_url VARCHAR(100),
+    image_url VARCHAR(300),
     event_url VARCHAR(100),
     date TIMESTAMP NOT NULL,
     is_remote BOOLEAN NOT NULL
