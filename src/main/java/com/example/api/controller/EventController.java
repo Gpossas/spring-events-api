62d485f5c72381becd75b03adc56f4c3305ae769
Guilherme D. Possas <possasg@gmail.com>
2024-06-20 06:11:28 -0300

    feat: add Get events in controller

--- src/main/java/com/example/api/controller/EventController.java
+++ src/main/java/com/example/api/controller/EventController.java
@@ -2,11 +2,14 @@ package com.example.api.controller;
 
 import com.example.api.domain.events.CreateEventDTO;
 import com.example.api.domain.events.Event;
+import com.example.api.domain.events.EventResponseDTO;
 import com.example.api.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/events")
@@ -29,5 +32,15 @@ public class EventController
     {
         CreateEventDTO event_dto = new CreateEventDTO(title, description, date, city, uf, is_remote, event_url, image);
         return ResponseEntity.ok(this.event_service.create_event(event_dto));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<EventResponseDTO>> get_events(
+            @RequestParam(defaultValue = "0") Integer page_number,
+            @RequestParam(defaultValue = "10") Integer page_size
+    )
+    {
+        List<EventResponseDTO> events = this.event_service.get_events(page_number, page_size);
+        return ResponseEntity.ok(events);
     }
 }
